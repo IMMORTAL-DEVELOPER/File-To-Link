04f291b95258f4db52fe3471f38f97a5e4071638
DEMIGOD-AKSHAY <91306144+DEMIGOD-AKSHAY@users.noreply.github.com>
2022-12-30 10:55:36 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/DEMIGOD-AKSHAY/File-To-Link.git /File-To-Link
+cd /File-To-Link
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
